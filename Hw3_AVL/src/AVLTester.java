d192e34aef88385357d00f4cdf062a38d6b2a13a
thoat <thoat@users.noreply.github.com>
2017-10-26 15:32:14 -0400

    finish up AVLTree, attempt AVLTester

--- Hw3_AVL/src/AVLTester.java
+++ Hw3_AVL/src/AVLTester.java
@@ -1,4 +1,18 @@
 
+/**
+ * While writing this class, we consulted the following websites regarding how to
+ * write test cases for exceptions (using JUnit):
+ * https://www.mkyong.com/unittest/junit-4-tutorial-2-expected-exception-test/
+ * https://github.com/junit-team/junit4/wiki/Exception-testing 
+ * https://blog.goyello.com/2015/10/01/different-ways-of-testing-exceptions-in-java-and-junit/
+ */
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
 public abstract class AVLTester {
 
 	public static boolean verifyAVL(StringTree toTest){
@@ -7,11 +21,77 @@ public abstract class AVLTester {
 		// You may assume that size() and isEmpty() return the correct values
 		// Other than this, do not assume anything about the tree toTest, including its start size.
                 
-                //makeEmpty();
+                
                 //insert(String key, String value);
                 //find(String key);
-		return false;
+                //makeEmpty();
+                boolean test1 = testMakeEmpty(toTest);
+                boolean test2 = testInsertNormal(toTest);
+                testInsertException(toTest);
+                boolean test4 = testFindNormal(toTest);
+                testFindException(toTest);
+                
+		return test1 && test2 && test4;
 	}
 	
 	// You may use as many static helper functions as you think are necessary
+        private static boolean testMakeEmpty(StringTree toTest) {
+            toTest.makeEmpty();
+            if (toTest.size() == 0)
+                return true;
+            return false;
+        }
+        
+        private static boolean testFindNormal(StringTree toTest) {
+            if (toTest.size() == 0)
+                return true;
+            if (toTest.find("66").equals("day"))
+                return true;
+            return false;
+        }
+        
+        private static boolean testInsertNormal(StringTree toTest) {
+            toTest.makeEmpty();
+            toTest.insert("20", "Today");
+            toTest.insert("1", "is");
+            toTest.insert("10", "a");
+            toTest.insert("5", "wonderful");
+            toTest.insert("66", "day");
+            toTest.insert("11", "isn't");
+            toTest.insert("4", "it");
+            
+            String actual = printTreeBFS(toTest);
+            String expected = "a it Today is wonderful isn't day ";
+            
+            return actual.equals(expected);                
+        }
+        
+        @Rule
+        public static ExpectedException thrown = ExpectedException.none();
+
+        @Test
+        public static void testInsertException(StringTree toTest) {
+            thrown.expect(IllegalArgumentException.class);
+            thrown.expectMessage("Duplicate key!");
+
+            toTest.insert("5", "?");
+        }
+        
+        @Test
+        public static void testFindException(StringTree toTest) throws NoSuchElementException {
+            thrown.expect(NoSuchElementException.class);
+            thrown.expectMessage("Key not found.");
+            
+            toTest.find("-9999");
+        }
+
+        private static String printTreeBFS(StringTree toTest) {
+            Iterator<String> iter = toTest.getBFSIterator();
+            String result = "";
+
+            while(iter.hasNext()) {
+                result = result + iter.next() + " ";
+            }
+            return result;
+        }
 }
--- Hw3_AVL/src/AVLTree.java
+++ Hw3_AVL/src/AVLTree.java
@@ -2,99 +2,79 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * This class represents an AVLTree.
+ * Note: methods getDeepestLevel(), buildListFromTree(), and printGivenOrder() 
+ * are referenced in some or all parts from 
+ * geeksforgeeks.org/level-order-tree-traversal
+ * @author Nova A. & Thoa T.
+ */
 public class AVLTree implements StringTree {
 
-    
 	private class AVLNode {
-		//Do not change these variable names
-		String key;
-		String value;
-		AVLNode left;
-		AVLNode right;
-                int height;
-		
-		//Place any additional fields you need here
-			
-		//TODO implement the node class here
-                public AVLNode(String k, String v) 
-                {
-                    key = k;
-                    value = v;
-                    left = null;
-                    right = null;
-                    height = 0;
-                }
+            //Do not change these variable names
+            String key;
+            String value;
+            AVLNode left;
+            AVLNode right;
+            int height;
+
+            public AVLNode(String k, String v) 
+            {
+                key = k;
+                value = v;
+                left = null;
+                right = null;
+                height = 0;
+            }   
 	}
 	
-	//Use this variable as your root
 	AVLNode root;
         int size;
-	
-	//You may use any additional fields here as you see fit
-        
-	public void makeEmpty() {
-                root = null;
-	}
-	
-	public int size() {
-		return size;
-	}
 
-	public void insert(String key, String value) {
+	public void makeEmpty() { root = null; }
+	
+	public int size() { return size; }
+        
+        public void insert(String key, String value) {
                 AVLNode aNode = new AVLNode(key, value);
                 insert(aNode, root);
-		
 	}
-        
-        private void checkBalance(AVLNode currPosition) {
-                if (currPosition.left.height - currPosition.right.height > 1) {
-                    //not balance, left is too tall, have to switch
-                }
-                else if (currPosition.left.height - currPosition.right.height < -1) {
-                    //not balance, right is too tall, have to switch
-                }
-                else
-                    return;
-        }
 
         private void insert(AVLNode aNode, AVLNode currPosition) {
                 if (currPosition == null) {
-                        currPosition = aNode;
-                        size++;
-                        checkBalance(currPosition);
-        }
-                else if (currPosition.key.compareTo(aNode.key) < 0) {  //if currentNode is smaller than the new node, go to the right
-                        currPosition.height++;
-                        insert(aNode, currPosition.right);
+                    currPosition = aNode;
+                    size++;
+                    updateHeightsInTree(root);
+                    checkBalanceInTree(root);
                 }
-                else if (currPosition.key.compareTo(aNode.key) > 0) {  //if currentNode is greater than the new node, go to the left
-                        currPosition.height++;
-                        insert(aNode, currPosition.left);
+                else if (aNode.key.compareTo(currPosition.key) > 0) {  //if the new node is greater than the currentNode, go to the right
+                    insert(aNode, currPosition.right);
+                }
+                else if (aNode.key.compareTo(currPosition.key) < 0) {  //if the new node is less than the currentNode, go to the left
+                    insert(aNode, currPosition.left);
                 }
                 else //if duplicate
-                        throw new IllegalArgumentException("Duplicate key!");
+                    throw new IllegalArgumentException("Duplicate key!");
     }
-
+        
+        /* Return the value affiliated with the String key. 
+           Throw a NoSuchElementException if the key is not in the AVLTree*/
 	public String find(String key) {
-		// TODO Return the value affiliated with the String key.
-		// Throw an ObjectNotFoundException if the key is not in the AVLTree
 		return find(key, root);
 	}
         
         private String find(String key, AVLNode currentNode) {
-            if (currentNode == null)
-                throw new NoSuchElementException("Key not found.");
-            if (key.compareTo(currentNode.key) == 0)
-                return currentNode.value;
-            if (key.compareTo(currentNode.key) > 0)
-                return find(key, currentNode.right);
-            else
-                return find(key, currentNode.left);
+                if (currentNode == null)
+                    throw new NoSuchElementException("Key not found.");
+                if (key.compareTo(currentNode.key) == 0)
+                    return currentNode.value;
+                if (key.compareTo(currentNode.key) > 0)
+                    return find(key, currentNode.right);
+                else
+                    return find(key, currentNode.left);
         }
 
- 
-       
-      
 	public Iterator<String> getBFSIterator() {
 		// TODO Only complete this section if you wish to attempt the 10 points EC
 		// This function should return a BFSIterator: Starter code provided below
@@ -106,69 +86,174 @@ public class AVLTree implements StringTree {
         
 	private class BFSIterator implements Iterator<String>{            
                 String curr;
-                String currPlus;
+                String prev;
                 ArrayList<String> arr;
             
 		public BFSIterator() {
-                    arr = buildListFromTree(root);
-                    currPlus = arr.get(0);
-                    curr = null;
+                        arr = buildListFromTree();
+                        curr = arr.get(0);
+                        prev = null;
                 }
         
                 public boolean hasNext() {
-			return currPlus != null;
+			return curr != null;
 		}
 
 		public String next() {
 			// TODO Return the next value in a BFS traversal of the tree
 			// It should start at the root and iterate through left children before right
-			if (currPlus == null) {
+			if (curr == null) {
                             throw new NoSuchElementException();
                         }
-                        String result = currPlus;
-                        curr = currPlus;
-                        int currPlusIndx = arr.indexOf(currPlus);
-                        currPlus = arr.get(currPlusIndx + 1);
-        
-        
+                        String result = curr;
+                        prev = curr;
+                        int currIndx = arr.indexOf(curr);
+                        curr = arr.get(currIndx + 1);
                         return result;
 		}
-                        
-                public ArrayList<String> buildListFromTree(AVLNode root) {
-                    ArrayList<String> arr = new ArrayList<String>();
-                    int h = height(root);
-                    int i;
-                    for (i = 1; i <= height; i++) {
-                        printGivenLevel(root, i, arr);
-                    }
-                    return arr;
-                }
-                
-                void printGivenLevel(AVLNode root, int level, ArrayList<String> arr) {
-                    if (root == null)
-                        return;
-                    if (level == 1) {
-                        //add this to the array list
-                        arr.add(root.value);
-                    }
-                    else if (level > 1) {
-                            printGivenLevel(root.left, level - 1, arr);
-                            printGivenLevel(root.right, level - 1, arr);
-                    }
-                }
-                
-                int height(AVLNode root) {
-                    if (root == null)
-                        return 0;
-                    else {
-                        /* Compute height of each subtree */
-                        int lh = height(root.left);
-                        int rh = height(root.right);
-                        
-                        if(lh > rh)
-                            return(lh + 1);
-                        else return(rh + 1);
-                    }
-                }
 	}
+        
+        /* Find the deepest level in a tree,
+         * which is depth of that tree + 1 */
+        public int getDeepestLevel(AVLNode root) {
+                if (root == null)
+                    return 0;
+                else {
+                    /* Compute deepest level of each subtree */
+                    int lh = getDeepestLevel(root.left);
+                    int rh = getDeepestLevel(root.right);
+
+                    if(lh > rh)
+                        return(lh + 1);
+                    else return(rh + 1);
+                }
+        }
+        
+        /* Update heights for all the nodes in tree */
+        private void updateHeightsInTree(AVLNode root) {
+                while (root != null) {
+                    root.height = getNodeHeight(root);
+                    updateHeightsInTree(root.left);
+                    updateHeightsInTree(root.right);
+                }
+        }
+        
+        /* Compute height for individual node */
+        private int getNodeHeight(AVLNode currNode) {
+                if (currNode == null)
+                    return -1;
+                if (currNode.left == null && currNode.right == null)
+                    return 0;
+                else {
+                    int lh = getNodeHeight(root.left);
+                    int rh = getNodeHeight(root.right);
+
+                    if (lh > rh)
+                        return lh + 1;
+                    else
+                        return rh + 1;
+                }
+        }
+   
+        private ArrayList<String> buildListFromTree() {
+                ArrayList<String> arr = new ArrayList<>();
+
+                //find the deepest level in this tree
+                int deepest = getDeepestLevel(root); 
+
+                for (int i = 1; i <= deepest; i++) {
+                    printGivenLevel(root, i, arr);
+                }
+                return arr;
+        }
+        
+        private void printGivenLevel(AVLNode currNode, int level, ArrayList<String> arr) {
+                if (currNode == null)
+                    return;
+                if (level == 1) {
+                    //add this node to the array list
+                    arr.add(currNode.value);
+                }
+                else if (level > 1) {
+                        printGivenLevel(currNode.left, level - 1, arr);
+                        printGivenLevel(currNode.right, level - 1, arr);
+                }
+        }
+        
+        private void checkBalanceInTree(AVLNode currPosition) {
+            
+                /* 0 : node does not have child; 
+                   1 : node has only one child; 
+                  -1 : node is null */
+                if (getNodeHeight(currPosition) <= 1) 
+                    return;
+                
+                int hdiff = getNodeHeight(currPosition.left) - getNodeHeight(currPosition.right);
+                if (hdiff <= 1 && hdiff >= -1) {
+                    checkBalanceInTree(currPosition.left);
+                    checkBalanceInTree(currPosition.right);
+                }
+                
+                else if (hdiff > 1) {
+                    //not balance, left is too tall                  
+                    //is it left-left or left-right?
+                    if (getNodeHeight(currPosition.left.left) - getNodeHeight(currPosition.left.right) >= 1)
+                        fixLeftLeft(currPosition, currPosition.left);
+                    else
+                        fixLeftRight(currPosition, currPosition.left);                    
+                }
+                else {
+                    //not balance, right is too tall
+                    //is it right-right or right-left?
+                    if (getNodeHeight(currPosition.right.right) - getNodeHeight(currPosition.right.left) >= 1)
+                        fixRightRight(currPosition, currPosition.right);
+                    else {
+                        fixRightLeft(currPosition, currPosition.right);
+                    }
+                }
+        }
+        
+        private void fixLeftLeft(AVLNode currPosition, AVLNode child) {
+                //left-left, so move child into parent
+                AVLNode newParent = child;
+                child.right = currPosition;
+                currPosition.left = newParent.right;
+                currPosition = newParent;
+        }
+        
+        private void fixRightRight(AVLNode currPosition, AVLNode child) {
+                //right-right, so move child into parent
+                AVLNode newParent = child;
+                child.left = currPosition;
+                currPosition.right = newParent.left;
+                currPosition = newParent;
+        }
+        
+        private void fixLeftRight(AVLNode currPosition, AVLNode child) {
+                //left-right, so move grandChild into parent
+                AVLNode grandChild = child.right;
+                AVLNode leftOfGrandChild = grandChild.left;
+                AVLNode rightOfGrandChild = grandChild.right;
+
+                AVLNode newParent = grandChild;
+                newParent.right = currPosition;
+                currPosition.left = rightOfGrandChild;
+                currPosition = newParent;
+                newParent.left = child;
+                child.right = leftOfGrandChild;
+        }
+        
+        private void fixRightLeft(AVLNode currPosition, AVLNode child) {
+                //right-left, so move grandChild into parent
+                AVLNode grandChild = child.left;
+                AVLNode rightOfGrandChild = grandChild.right;
+                AVLNode leftOfGrandChild = grandChild.left;
+
+                AVLNode newParent = grandChild;
+                newParent.left = currPosition;
+                currPosition.right = leftOfGrandChild;
+                currPosition = newParent;
+                newParent.right = child;
+                child.left = rightOfGrandChild;
+        }
 }
